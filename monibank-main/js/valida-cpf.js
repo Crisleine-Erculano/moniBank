94cd530847c4c86421432ffd0b7b5795048bb133
Crisleine Erculano <candidocrislaine576@gmail.com>
2023-06-12 13:05:27 -0300

    Validando primerio dígito do CPF.

--- monibank-main/js/valida-cpf.js
+++ monibank-main/js/valida-cpf.js
@@ -1,6 +1,7 @@
 export  function ehUmCPF(campo) {
    const cpf = campo.value.replace(/\.|-/g, "");
    validaNumerosRepetidos(cpf);
+   validaPrimeiroDigito(cpf);
    console.log(validaNumerosRepetidos(cpf))
 }
 function validaNumerosRepetidos(cpf) {
@@ -17,4 +18,19 @@ function validaNumerosRepetidos(cpf) {
    '99999999999'
    ]
    return numerosRepetidos.includes(cpf);
+}
+
+function validaPrimeiroDigito(cpf){
+   let soma = 0;
+   let multiplicador = 10;
+
+   for(let tamanho = 0; tamanho < 9; tamanho++){
+      soma += cpf[tamanho] * multiplicador;
+      multiplicador--;
+   }
+   soma = (soma * 10) % 11;
+   if(soma == 10 || soma == 11){
+      soma= 0;
+   }
+   return soma != cpf[9];
 }
