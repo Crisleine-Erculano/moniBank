600c9f2b0af542c20226497157ffabaa98dd70b7
Crisleine Erculano <candidocrislaine576@gmail.com>
2023-06-12 13:58:26 -0300

    Validando maior idade.

--- monibank-main/js/script.js
+++ monibank-main/js/script.js
@@ -11,5 +11,8 @@ camposDoFormulario.forEach((campo)=>{
    if(campo.name == "cpf" && campo.value.length >= 11){
       ehUmCPF(campo);
    }
+   if(campo.name == "aniversario" && campo.value !== ""){
+      ehMaiorDeIdade(campo);
+   }
 
  }
